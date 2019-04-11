ebf0c110a665fd1b23c2f3e7b19b9148ad8cea08
swathithangaraj <swathithangaraj26@gmail.com>
2019-04-11 11:31:17 +0530

    message

--- name.py
+++ name.py
@@ -2,8 +2,10 @@ for i in range(10):
     print(f'{i}')
     print("steveJob")
     print("elonMusk")
+    print("nisha")
 <<<<<<< HEAD
     print("raptor")
 =======
     print("Mark") 
 >>>>>>> b6f6477b6f6d7595e443836d5b2c9394ee122138
+
